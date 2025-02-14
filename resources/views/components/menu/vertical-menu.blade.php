900a9de40fd0c3e534f95b3f8194288ef04f3519
Miguel <miguelgomez29117@hotmail.com>
2025-02-14 14:14:56 -0500

    API_CATASTRO3

--- resources/views/components/menu/vertical-menu.blade.php
+++ resources/views/components/menu/vertical-menu.blade.php
@@ -63,8 +63,8 @@
                 <div class="heading"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg><span>APPLICATIONS</span></div>
             </li>
             -->
-         @if(!Auth::user()->rol_id == 6)
-         <li class="menu ">
+            @if(Auth::user()->rol_id != 6)
+            <li class="menu ">
             <a href="/crear-folio" aria-expanded="false" class="dropdown-toggle">
                <div class="">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-tag">
@@ -170,7 +170,7 @@
             </a>
          </li>
          @endif
-         @if(!Auth::user()->rol_id == 6)
+         @if(Auth::user()->rol_id != 6)
          <li class="menu ">
             <a href="/inbox" aria-expanded="false" class="dropdown-toggle">
                <div class="">
